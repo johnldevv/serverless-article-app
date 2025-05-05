20cbe5a8f5138de70852682871bc68b169e0ac33
John <techwithjohnblog@gmail.com>
2025-05-06 06:53:44 +0800

    Add API Gateway config and connect POST /articles to Lambda

--- infra/environments/staging/main.tf
+++ infra/environments/staging/main.tf
@@ -28,3 +28,41 @@ resource "aws_lambda_function" "post_article" {
   filename      = "${path.module}/../../../dist/postArticle.zip"
   source_code_hash = filebase64sha256("${path.module}/../../../dist/postArticle.zip")
 }
+
+# API Gateway (HTTP API)
+resource "aws_apigatewayv2_api" "articles_api" {
+  name          = "articles-api-staging"
+  protocol_type = "HTTP"
+}
+
+# Integration: API Gateway → Lambda
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id           = aws_apigatewayv2_api.articles_api.id
+  integration_type = "AWS_PROXY"
+  integration_uri  = aws_lambda_function.post_article.invoke_arn
+  integration_method = "POST"
+  payload_format_version = "2.0"
+}
+
+# Route: POST /articles
+resource "aws_apigatewayv2_route" "post_articles" {
+  api_id    = aws_apigatewayv2_api.articles_api.id
+  route_key = "POST /articles"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+# Deployment stage
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.articles_api.id
+  name        = "staging"
+  auto_deploy = true
+}
+
+# Allow API Gateway to invoke Lambda
+resource "aws_lambda_permission" "allow_apigw_invoke" {
+  statement_id  = "AllowInvokeFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.post_article.arn
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.articles_api.execution_arn}/*/*"
+}
